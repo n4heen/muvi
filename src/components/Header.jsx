36f3fd81ced4d2ba0444e5f72af9d98f2cbfdd8c
Naheen Habib <naheenhabib@outlook.com>
2022-11-20 14:24:06 +1100

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import "./Header.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 export default function Header() {
     return (
         <header>
+            <Router>
             <h1 className='logo'>Muvi</h1>
+            <li >
+                <Link to="/cart" >
+                <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+                </Link>
+            </li>
+            </Router>
         </header>
     )
 }
