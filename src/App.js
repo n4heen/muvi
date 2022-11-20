f32cf4e87a17807f9a0bcc9caddd57f330a7934c
Naheen Habib <naheenhabib@outlook.com>
2022-11-20 13:52:02 +1100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Nav from "./components/Nav";
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="App">
-<Nav/>
+<Header/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Header.css";
+export default function Header() {
+    return (
+        <header>
+            <h1 className='logo'>Muvi</h1>
+        </header>
+    )
+}
