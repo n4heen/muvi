a03a9d0bb2b01ebbe5b320813d3c9aba7ff66fbc
Naheen Habib <naheenhabib@outlook.com>
2022-11-23 19:35:26 +1100

    new commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import requests from '../utils/requests'
+import { useNavigate } from "react-router-dom";
+import "./Nav.css";
 
 export default function Nav() {
+    
     return (
         <nav >
-            <div >
+            <div className='container' >
                 {Object.entries(requests).map(([key, { title, url }]) => (
                     <h2
                         key={key}
